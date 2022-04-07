04ff43e0c8fdbb0372f590646364fb5a4ac84746
Falk Franco <88750038+FalkFranco@users.noreply.github.com>
2022-04-07 03:08:09 -0300

    module Paciente

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,25 @@
+
+
+const Paciente = () => {
+  return (
+    <div className="mt-5 bg-white shadow-md m-3 px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}
+        <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{' '}
+        <span className="font-normal normal-case">Franco</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}
+        <span className="font-normal normal-case">Correo@correo.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta:{' '}
+        <span className="font-normal normal-case">10-12-22</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{' '}
+        <span className="font-normal normal-case">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex sit minus, repudiandae eligendi pariatur assumenda officiis facilis neque aperiam praesentium quidem deserunt libero, iusto voluptatibus tempora ad temporibus dignissimos possimus.</span>
+        </p>
+    </div>
+  )
+}
+
+export default Paciente
